32f66646ec10024cee2b45cc6278138385f8493d
matthewpblock <matthewpblock@gmail.com>
2025-01-28 17:57:27 -1000

    rename files to fit assigned convention

---
+++ block_process_csv.py
@@ -0,0 +1,31 @@
+#####################################
+# Import Modules
+#####################################
+
+# Import from Python Standard Library
+import pathlib
+import csv
+import statistics
+
+# Import from local project modules
+from utils_logger import logger
+
+#####################################
+# Declare Global Variables
+#####################################
+fetched_folder_name: str = "data"
+processed_folder_name: str = "processed"
+
+#####################################
+# Define Functions
+#####################################
+# Analyze Fadeaways
+def analyze_fadeaways
+#####################################
+# Main Execution
+#####################################
+
+if __name__ == "__main__":
+    logger.info("Starting CSV processing...")
+    process_csv_file()
+    logger.info("CSV processing complete.")
